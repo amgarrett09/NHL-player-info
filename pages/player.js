85f99066ef5358243e8c4ca61e94162c770cce86
Alex Garrett <agarrettR8@gmail.com>
2019-03-06 18:33:18 -0500

    added some more basic info on player pages

--- pages/player.js
+++ pages/player.js
@@ -5,23 +5,44 @@ import SeasonStats from '../components/SeasonStats';
 import PlayoffStats from '../components/PlayoffStats';
 
 const Player = ({
-  id, name, currentTeam, position, shootsCatches,
-}) => (
-  <main>
-    <section>
-      <h1>{name}</h1>
-      <p><strong>{currentTeam}</strong></p>
-      <p>{`Position: ${position} • Shoots/Catches: ${shootsCatches}`}</p>
-    </section>
+  id,
+  name,
+  currentTeam,
+  position,
+  shootsCatches,
+  age,
+  height,
+  weight,
+  nationality,
+}) => {
+  if (id === '') {
+    return (
+      <main>
+        <h1>Invalid player id</h1>
+      </main>
+    );
+  }
 
-    <section>
-      <SeasonStats id={id} position={position} />
-    </section>
-    <section>
-      <PlayoffStats id={id} position={position} />
-    </section>
-  </main>
-);
+  return (
+    <main>
+      <section>
+        <h1>{name}</h1>
+        <p>
+          <strong>{currentTeam}</strong>
+        </p>
+        <p>{`Position: ${position} • Shoots/Catches: ${shootsCatches}`}</p>
+        <p>{`Age ${age} • ${height} • ${weight} lbs • ${nationality}`}</p>
+      </section>
+
+      <section>
+        <SeasonStats id={id} position={position} />
+      </section>
+      <section>
+        <PlayoffStats id={id} position={position} />
+      </section>
+    </main>
+  );
+};
 
 Player.propTypes = {
   id: PropTypes.string,
@@ -29,6 +50,10 @@ Player.propTypes = {
   currentTeam: PropTypes.string,
   position: PropTypes.string,
   shootsCatches: PropTypes.string,
+  age: PropTypes.number,
+  height: PropTypes.string,
+  weight: PropTypes.number,
+  nationality: PropTypes.string,
 };
 
 Player.defaultProps = {
@@ -37,6 +62,10 @@ Player.defaultProps = {
   currentTeam: '',
   position: '',
   shootsCatches: '',
+  age: undefined,
+  height: '',
+  weight: undefined,
+  nationality: '',
 };
 
 Player.getInitialProps = async ({ query }) => {
@@ -47,13 +76,17 @@ Player.getInitialProps = async ({ query }) => {
     const json = await res.json();
     const [person] = json.people;
 
-    return ({
+    return {
       id: query.id,
       name: person.fullName,
       currentTeam: person.currentTeam.name,
       position: person.primaryPosition.name,
       shootsCatches: person.shootsCatches,
-    });
+      age: person.currentAge,
+      height: person.height,
+      weight: person.weight,
+      nationality: person.nationality,
+    };
   } catch {
     return {};
   }
