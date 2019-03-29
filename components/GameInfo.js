70120799221435f4e9d47e79c460bab7b9823c6d
Alex Garrett <agarrettR8@gmail.com>
2019-03-29 02:34:01 -0400

    feat: added game info component

---
+++ components/GameInfo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import teamAbbreviations from '../constants/constants';
+import '../css/game-info.css';
+
+const GameInfo = ({
+  homeTeam, awayTeam, homeScore, awayScore, gameState,
+}) => (
+  <div className="game-info">
+    <div className="game-info__scores">
+      <div className="row">
+        <div className="col s6">
+          {teamAbbreviations[awayTeam] || awayTeam}
+        </div>
+        <div className="col s6">
+          {awayScore}
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col s6">
+          {teamAbbreviations[homeTeam] || awayTeam}
+        </div>
+        <div className="col s6">
+          {homeScore}
+        </div>
+      </div>
+    </div>
+
+    <div className="game-info__game-state">
+      {gameState}
+    </div>
+  </div>
+);
+
+GameInfo.propTypes = {
+  homeTeam: PropTypes.string.isRequired,
+  awayTeam: PropTypes.string.isRequired,
+  homeScore: PropTypes.number.isRequired,
+  awayScore: PropTypes.number.isRequired,
+  gameState: PropTypes.string.isRequired,
+};
+
+export default GameInfo;
