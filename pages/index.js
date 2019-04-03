d9736183c6b6900ffd118be00b8adcd8995ef1cc
Alex Garrett <agarrettR8@gmail.com>
2019-04-03 10:38:51 -0400

    feat: added entrance animation to carousel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 
+import { Spring } from 'react-spring';
 import PlayerList from '../components/PlayerList';
 import DivisionStandings from '../components/DivisionStandings';
 import withGameState from '../components/higher-order/withGameState';
@@ -18,7 +19,10 @@ const Index = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       <title>Hockey Scrub</title>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
     </Head>
     <main>
       <div className="container">
@@ -27,17 +31,25 @@ const Index = () => (
         </div>
         <PlayerList />
         <Desktop>
-          <section>
+          <section className="index__carousel">
             <h2>Today&#39;s Games</h2>
             <div className="centered">
-              <TodaysGames />
+              <Spring
+                from={{ transform: 'translateX(-100%)' }}
+                to={{ transform: 'translateX(0)' }}
+              >
+                {springprops => (
+                  <div style={springprops}>
+                    <TodaysGames />
+                  </div>
+                )}
+              </Spring>
             </div>
           </section>
         </Desktop>
         <DivisionStandings />
       </div>
     </main>
-
   </React.Fragment>
 );
 
