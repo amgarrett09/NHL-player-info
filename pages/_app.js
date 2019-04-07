dc1abe36668d4a15d26d00cf8176e5ffe2c1aaf0
Alex Garrett <agarrettR8@gmail.com>
2019-04-06 23:27:17 -0400

    feat: re-added page transition animations

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import { PageTransition } from 'next-page-transitions';
 import Layout from '../components/Layout';
 import '../css/materialize.css';
 
@@ -20,8 +21,30 @@ class MyApp extends App {
     return (
       <Container>
         <Layout>
-          <Component {...pageProps} />
+          <PageTransition timeout={300} classNames="page-transition">
+            <Component {...pageProps} />
+          </PageTransition>
         </Layout>
+        <style jsx global>
+          {`
+          .page-transition-enter {
+            opacity: 0;
+            transform: translateY(-20px);
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transform: translateY(0px);
+            transition: opacity 300ms, transform 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}
+        </style>
       </Container>
     );
   }
