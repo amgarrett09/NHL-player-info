af08200f8f56b149b45a2af094b0a2f5c28f1188
Alex Garrett <agarrettR8@gmail.com>
2019-04-06 22:17:56 -0400

    Revert "feat: added animations to page transitions"
    This reverts commit f821b5cba9d850b38034863895764b6c8510c2af.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import App, { Container } from 'next/app';
-import { PageTransition } from 'next-page-transitions';
 import Layout from '../components/Layout';
 import '../css/materialize.css';
 
@@ -16,38 +15,13 @@ class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps, router } = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
       <Container>
         <Layout>
-          <PageTransition timeout={300} classNames="page-transition">
-            <Component {...pageProps} key={router.route} />
-          </PageTransition>
+          <Component {...pageProps} />
         </Layout>
-        <style jsx global>
-          {`
-          .page-transition-enter {
-            opacity: 0;
-            transform: translateY(-20px);
-          }
-          .page-transition-enter-active {
-            opacity: 1;
-            transform: translateY(0%);
-            transition: opacity 200ms, transform 300ms;
-          }
-          .page-transition-exit {
-            opacity: 1;
-            transform: translateY(0%);
-          }
-          .page-transition-exit-active {
-            opacity: 0;
-            transform: translateY(20px);
-            transition: opacity 200ms, transform 300ms;
-          }
-        `}
-
-        </style>
       </Container>
     );
   }
