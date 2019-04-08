dca3b0105f2ec3cd8a5cc3c026257076be210e05
Alex Garrett <agarrettR8@gmail.com>
2019-04-08 17:33:30 -0400

    fix: disabled initial page transition

--- pages/_app.js
+++ pages/_app.js
@@ -19,56 +19,60 @@ class MyApp extends App {
   // State for react context
   state = {
     loading: false,
-  }
+  };
 
   setLoading = () => {
     this.setState({
       loading: true,
     });
-  }
+  };
 
   unsetLoading = () => {
     this.setState({
       loading: false,
     });
-  }
+  };
 
   render() {
     const { Component, pageProps, router } = this.props;
 
     return (
       <Container>
-        <LoadingContext.Provider value={{
-          loading: this.state.loading,
-          setLoading: this.setLoading,
-          unsetLoading: this.unsetLoading,
-        }}
+        <LoadingContext.Provider
+          value={{
+            loading: this.state.loading,
+            setLoading: this.setLoading,
+            unsetLoading: this.unsetLoading,
+          }}
         >
           <Layout>
-            <PageTransition timeout={200} classNames="page-transition">
+            <PageTransition
+              timeout={200}
+              classNames="page-transition"
+              skipInitialTransition
+            >
               <Component {...pageProps} key={router.route} />
             </PageTransition>
           </Layout>
           <style jsx global>
             {`
-          .page-transition-enter {
-            opacity: 0;
-            transform: translateY(-20px);
-          }
-          .page-transition-enter-active {
-            opacity: 1;
-            transform: translateY(0px);
-            transition: opacity 200ms, transform 200ms;
-          }
-          .page-transition-exit {
-            opacity: 1;
-          }
-          .page-transition-exit-active {
-            opacity: 0;
-            transition: opacity 200ms;
-          }
-        `}
-
+              .page-transition-enter {
+                opacity: 0;
+                transform: translateY(-20px);
+              }
+              .page-transition-enter-active {
+                opacity: 1;
+                transform: translateY(0px);
+                transition: opacity 200ms, transform 200ms;
+              }
+              .page-transition-exit {
+                opacity: 1;
+              }
+              .page-transition-exit-active {
+                opacity: 0;
+                transition: opacity 200ms;
+              }
+            `}
           </style>
         </LoadingContext.Provider>
       </Container>
