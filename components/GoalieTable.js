f724871de39004dca6b0d755838684fed8ee056a
Alex Garrett <agarrettR8@gmail.com>
2019-06-11 20:27:52 -0400

    can now sort season and playoff stat tables

--- components/GoalieTable.js
+++ components/GoalieTable.js
@@ -1,76 +1,309 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import convertSeason from '../lib/convert-season';
-
 import teamAbbreviations from '../constants/constants';
 
-const GoalieTable = ({ stats, career }) => (
-  <table className="table is-narrow is-hoverable is-fullwidth">
-    <thead>
-      <tr>
-        <th>Season</th>
-        <th>Team</th>
-        <th><abbr title="Games Played">GP</abbr></th>
-        <th><abbr title="Games Started">GS</abbr></th>
-        <th><abbr title="Wins">W</abbr></th>
-        <th><abbr title="Losses">L</abbr></th>
-        <th><abbr title="Ties">T</abbr></th>
-        <th><abbr title="Overtime Losses">OTL</abbr></th>
-        <th><abbr title="Shots Against">SA</abbr></th>
-        <th><abbr title="Goals Against">GA</abbr></th>
-        <th><abbr title="Goals Against Average">GAA</abbr></th>
-        <th><abbr title="Saves">S</abbr></th>
-        <th><abbr title="Save Percentage">S%</abbr></th>
-        <th><abbr title="Shutouts">SO</abbr></th>
-        <th><abbr title="Minutes">MIN</abbr></th>
-      </tr>
-    </thead>
+const sortingFunctions = {
+  Season(a, b) {
+    return a.season - b.season;
+  },
+  GP(a, b) {
+    return b.stat.games - a.stat.games;
+  },
+  GS(a, b) {
+    return b.stat.gamesStarted - a.stat.gamesStarted;
+  },
+  W(a, b) {
+    return b.stat.wins - a.stat.wins;
+  },
+  L(a, b) {
+    return b.stat.losses - a.stat.losses;
+  },
+  T(a, b) {
+    return b.stat.ties - a.stat.ties;
+  },
+  OTL(a, b) {
+    return b.stat.ot - a.stat.ot;
+  },
+  SA(a, b) {
+    return b.stat.shotsAgainst - a.stat.shotsAgainst;
+  },
+  GA(a, b) {
+    return b.stat.goalsAgainst - a.stat.goalsAgainst;
+  },
+  GAA(a, b) {
+    return b.stat.goalAgainstAverage - a.stat.goalAgainstAverage;
+  },
+  S(a, b) {
+    return b.stat.saves - a.stat.saves;
+  },
+  // eslint-disable-next-line func-names
+  'S%': function (a, b) {
+    return b.stat.savePercentage - a.stat.savePercentage;
+  },
+  SO(a, b) {
+    return b.stat.shutouts - a.stat.shutouts;
+  },
+  MIN(a, b) {
+    return b.stat.timeOnIce - a.stat.timeOnIce;
+  },
+};
 
-    <tbody>
-      {stats.map((obj, i) => (
-        <tr key={obj.season + obj.team.name + i.toString()}>
-          <td>{convertSeason(obj.season)}</td>
-          <td>{teamAbbreviations[obj.team.name] || obj.team.name}</td>
-          <td>{obj.stat.games}</td>
-          <td>{obj.stat.gamesStarted}</td>
-          <td>{obj.stat.wins}</td>
-          <td>{obj.stat.losses}</td>
-          <td>{obj.stat.ties}</td>
-          <td>{obj.stat.ot}</td>
-          <td>{obj.stat.shotsAgainst}</td>
-          <td>{obj.stat.goalsAgainst}</td>
-          <td>{obj.stat.goalAgainstAverage}</td>
-          <td>{obj.stat.saves}</td>
-          <td>{obj.stat.savePercentage}</td>
-          <td>{obj.stat.shutouts}</td>
-          <td>{obj.stat.timeOnIce}</td>
-        </tr>
-      ))}
-      <tr>
-        <td><strong>Career</strong></td>
-        <td />
-        <td><strong>{career.games}</strong></td>
-        <td><strong>{career.gamesStarted}</strong></td>
-        <td><strong>{career.wins}</strong></td>
-        <td><strong>{career.losses}</strong></td>
-        <td><strong>{career.ties}</strong></td>
-        <td><strong>{career.ot}</strong></td>
-        <td><strong>{career.shotsAgainst}</strong></td>
-        <td><strong>{career.goalsAgainst}</strong></td>
-        <td><strong>{career.goalAgainstAverage}</strong></td>
-        <td><strong>{career.saves}</strong></td>
-        <td><strong>{career.savePercentage}</strong></td>
-        <td><strong>{career.shutouts}</strong></td>
-        <td><strong>{career.timeOnIce}</strong></td>
-      </tr>
-    </tbody>
-  </table>
-);
+const GoalieTable = ({ stats, career }) => {
+  const [sortedStats, setSortedStats] = useState(stats);
+  const [currentlySorted, setCurrentlySorted] = useState('');
+
+  // Sort the table by a particular stat when you click on the column's title.
+  const sortBy = (e) => {
+    e.preventDefault();
+
+    const colTitle = e.target.innerText;
+
+    if (currentlySorted === colTitle) {
+      const newStats = sortedStats.slice(0, sortedStats.length).reverse();
+
+      setSortedStats(newStats);
+    } else {
+      const newStats = sortedStats
+        .slice(0, sortedStats.length)
+        .sort(sortingFunctions[colTitle]);
+
+      setSortedStats(newStats);
+      setCurrentlySorted(colTitle);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <table className="table is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                Season
+              </button>
+            </th>
+            <th>Team</th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">GP</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">GS</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">W</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">L</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">T</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">OTL</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">SA</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">GA</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">GAA</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">S</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">S%</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">SO</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">MIN</abbr>
+              </button>
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {sortedStats.map((obj, i) => (
+            <tr key={obj.season + obj.team.name + i.toString()}>
+              <td
+                className={currentlySorted === 'Season' ? 'stat-cell--sorted' : ''}
+              >
+                {convertSeason(obj.season)}
+              </td>
+              <td>{teamAbbreviations[obj.team.name] || obj.team.name}</td>
+              <td
+                className={currentlySorted === 'GP' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.games}
+              </td>
+              <td
+                className={currentlySorted === 'GS' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.gamesStarted}
+              </td>
+              <td
+                className={currentlySorted === 'W' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.wins}
+              </td>
+              <td
+                className={currentlySorted === 'L' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.losses}
+              </td>
+              <td
+                className={currentlySorted === 'T' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.ties}
+              </td>
+              <td
+                className={currentlySorted === 'OTL' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.ot}
+              </td>
+              <td
+                className={currentlySorted === 'SA' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.shotsAgainst}
+              </td>
+              <td
+                className={currentlySorted === 'GA' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.goalsAgainst}
+              </td>
+              <td
+                className={currentlySorted === 'GAA' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.goalAgainstAverage}
+              </td>
+              <td
+                className={currentlySorted === 'S' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.saves}
+              </td>
+              <td
+                className={currentlySorted === 'S%' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.savePercentage}
+              </td>
+              <td
+                className={currentlySorted === 'SO' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.shutouts}
+              </td>
+              <td
+                className={currentlySorted === 'MIN' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.timeOnIce}
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td>
+              <strong>Career</strong>
+            </td>
+            <td />
+            <td>
+              <strong>{career.games}</strong>
+            </td>
+            <td>
+              <strong>{career.gamesStarted}</strong>
+            </td>
+            <td>
+              <strong>{career.wins}</strong>
+            </td>
+            <td>
+              <strong>{career.losses}</strong>
+            </td>
+            <td>
+              <strong>{career.ties}</strong>
+            </td>
+            <td>
+              <strong>{career.ot}</strong>
+            </td>
+            <td>
+              <strong>{career.shotsAgainst}</strong>
+            </td>
+            <td>
+              <strong>{career.goalsAgainst}</strong>
+            </td>
+            <td>
+              <strong>{career.goalAgainstAverage}</strong>
+            </td>
+            <td>
+              <strong>{career.saves}</strong>
+            </td>
+            <td>
+              <strong>{career.savePercentage}</strong>
+            </td>
+            <td>
+              <strong>{career.shutouts}</strong>
+            </td>
+            <td>
+              <strong>{career.timeOnIce}</strong>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <style jsx>
+        {`
+          .sort-button {
+            padding: 0;
+            margin: 0;
+            border: none;
+            font-weight: bold;
+            background: transparent;
+          }
+          .sort-button:hover {
+            cursor: pointer;
+          }
+
+          .stat-cell--sorted {
+            background-color: yellow;
+          }
+        `}
+      </style>
+    </React.Fragment>
+  );
+};
 
 GoalieTable.propTypes = {
-  stats: PropTypes.arrayOf(PropTypes.shape({
-    season: PropTypes.string,
-  })),
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      season: PropTypes.string,
+    }),
+  ),
   career: PropTypes.shape({
     stat: PropTypes.shape({
       timeOnIce: PropTypes.string,
--- components/SkaterTable.js
+++ components/SkaterTable.js
@@ -1,79 +1,327 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import convertSeason from '../lib/convert-season';
-
 import teamAbbreviations from '../constants/constants';
 
-const SkaterTable = ({ stats, career }) => (
-  <table className="table is-narrow is-hoverable is-fullwidth">
-    <thead>
-      <tr>
-        <th>Season</th>
-        <th>Team</th>
-        <th><abbr title="Games Played">GP</abbr></th>
-        <th><abbr title="Goals">G</abbr></th>
-        <th><abbr title="Assists">A</abbr></th>
-        <th><abbr title="Points">P</abbr></th>
-        <th><abbr title="Plus / Minus">+/-</abbr></th>
-        <th><abbr title="Penalty in Minutes">PIM</abbr></th>
-        <th><abbr title="Powerplay Goals">PPG</abbr></th>
-        <th><abbr title="Powerplay Points">PPP</abbr></th>
-        <th><abbr title="Shorthanded Goals">SHG</abbr></th>
-        <th><abbr title="Shorthanded Points">SHP</abbr></th>
-        <th><abbr title="Game Winning Goals">GWG</abbr></th>
-        <th><abbr title="Overtime Goals">OTG</abbr></th>
-        <th><abbr title="Shots">S</abbr></th>
-        <th><abbr title="Shooting Percentage">S%</abbr></th>
-      </tr>
-    </thead>
+const sortingFunctions = {
+  Season(a, b) {
+    return a.season - b.season;
+  },
+  GP(a, b) {
+    return b.stat.games - a.stat.games;
+  },
+  G(a, b) {
+    return b.stat.goals - a.stat.goals;
+  },
+  A(a, b) {
+    return b.stat.assists - a.stat.assists;
+  },
+  P(a, b) {
+    return b.stat.points - a.stat.points;
+  },
+  // eslint-disable-next-line func-names
+  '+/-': function (a, b) {
+    return b.stat.plusMinus - a.stat.plusMinus;
+  },
+  PIM(a, b) {
+    return b.stat.pim - a.stat.pim;
+  },
+  PPG(a, b) {
+    return b.stat.powerPlayGoals - a.stat.powerPlayGoals;
+  },
+  PPP(a, b) {
+    return b.stat.powerPlayPoints - a.stat.powerPlayPoints;
+  },
+  SHG(a, b) {
+    return b.stat.shortHandedGoals - a.stat.shortHandedGoals;
+  },
+  SHP(a, b) {
+    return b.stat.shortHandedPoints - a.stat.shortHandedPoints;
+  },
+  GWG(a, b) {
+    return b.stat.gameWinningGoals - a.stat.gameWinningGoals;
+  },
+  OTG(a, b) {
+    return b.stat.overTimeGoals - a.stat.overTimeGoals;
+  },
+  S(a, b) {
+    return b.stat.shots - a.stat.shots;
+  },
+  // eslint-disable-next-line func-names
+  'S%': function (a, b) {
+    return b.stat.shotPct - a.stat.shotPct;
+  },
+};
 
-    <tbody>
-      {stats.map((obj, i) => (
-        <tr key={obj.season + obj.team.name + i.toString()}>
-          <td>{convertSeason(obj.season)}</td>
-          <td>{teamAbbreviations[obj.team.name] || obj.team.name}</td>
-          <td>{obj.stat.games}</td>
-          <td>{obj.stat.goals}</td>
-          <td>{obj.stat.assists}</td>
-          <td>{obj.stat.points}</td>
-          <td>{obj.stat.plusMinus}</td>
-          <td>{obj.stat.pim}</td>
-          <td>{obj.stat.powerPlayGoals}</td>
-          <td>{obj.stat.powerPlayPoints}</td>
-          <td>{obj.stat.shortHandedGoals}</td>
-          <td>{obj.stat.shortHandedPoints}</td>
-          <td>{obj.stat.gameWinningGoals}</td>
-          <td>{obj.stat.overTimeGoals}</td>
-          <td>{obj.stat.shots}</td>
-          <td>{obj.stat.shotPct}</td>
-        </tr>
-      ))}
-      <tr>
-        <td><strong>Career</strong></td>
-        <td />
-        <td><strong>{career.games}</strong></td>
-        <td><strong>{career.goals}</strong></td>
-        <td><strong>{career.assists}</strong></td>
-        <td><strong>{career.points}</strong></td>
-        <td><strong>{career.plusMinus}</strong></td>
-        <td><strong>{career.pim}</strong></td>
-        <td><strong>{career.powerPlayGoals}</strong></td>
-        <td><strong>{career.powerPlayPoints}</strong></td>
-        <td><strong>{career.shortHandedGoals}</strong></td>
-        <td><strong>{career.shortHandedPoints}</strong></td>
-        <td><strong>{career.gameWinningGoals}</strong></td>
-        <td><strong>{career.overTimeGoals}</strong></td>
-        <td><strong>{career.shots}</strong></td>
-        <td><strong>{career.shotPct}</strong></td>
-      </tr>
-    </tbody>
-  </table>
-);
+const SkaterTable = ({ stats, career }) => {
+  const [sortedStats, setSortedStats] = useState(stats);
+  const [currentlySorted, setCurrentlySorted] = useState('');
+
+  // Sort the table by a particular stat when you click on the column's title.
+  const sortBy = (e) => {
+    e.preventDefault();
+
+    const colTitle = e.target.innerText;
+
+    if (currentlySorted === colTitle) {
+      const newStats = sortedStats.slice(0, sortedStats.length).reverse();
+
+      setSortedStats(newStats);
+    } else {
+      const newStats = sortedStats
+        .slice(0, sortedStats.length)
+        .sort(sortingFunctions[colTitle]);
+
+      setSortedStats(newStats);
+      setCurrentlySorted(colTitle);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <table className="table is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                Season
+              </button>
+            </th>
+            <th>Team</th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Games Played">GP</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Goals">G</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Assists">A</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Points">P</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Plus / Minus">+/-</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Penalty in Minutes">PIM</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Powerplay Goals">PPG</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Powerplay Points">PPP</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Shorthanded Goals">SHG</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Shorthanded Points">SHP</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Game Winning Goals">GWG</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Overtime Goals">OTG</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Shots">S</abbr>
+              </button>
+            </th>
+            <th>
+              <button type="button" className="sort-button" onClick={sortBy}>
+                <abbr title="Shooting Percentage">S%</abbr>
+              </button>
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {sortedStats.map((obj, i) => (
+            <tr key={obj.season + obj.team.name + i.toString()}>
+              <td
+                className={
+                  currentlySorted === 'Season' ? 'stat-cell--sorted' : ''
+                }
+              >
+                {convertSeason(obj.season)}
+              </td>
+              <td>{teamAbbreviations[obj.team.name] || obj.team.name}</td>
+              <td
+                className={currentlySorted === 'GP' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.games}
+              </td>
+              <td
+                className={currentlySorted === 'G' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.goals}
+              </td>
+              <td
+                className={currentlySorted === 'A' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.assists}
+              </td>
+              <td
+                className={currentlySorted === 'P' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.points}
+              </td>
+              <td
+                className={currentlySorted === '+/-' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.plusMinus}
+              </td>
+              <td
+                className={currentlySorted === 'PIM' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.pim}
+              </td>
+              <td
+                className={currentlySorted === 'PPG' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.powerPlayGoals}
+              </td>
+              <td
+                className={currentlySorted === 'PPP' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.powerPlayPoints}
+              </td>
+              <td
+                className={currentlySorted === 'SHG' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.shortHandedGoals}
+              </td>
+              <td
+                className={currentlySorted === 'SHP' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.shortHandedPoints}
+              </td>
+              <td
+                className={currentlySorted === 'GWG' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.gameWinningGoals}
+              </td>
+              <td
+                className={currentlySorted === 'OTG' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.overTimeGoals}
+              </td>
+              <td
+                className={currentlySorted === 'S' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.shots}
+              </td>
+              <td
+                className={currentlySorted === 'S%' ? 'stat-cell--sorted' : ''}
+              >
+                {obj.stat.shotPct}
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td>
+              <strong>Career</strong>
+            </td>
+            <td />
+            <td>
+              <strong>{career.games}</strong>
+            </td>
+            <td>
+              <strong>{career.goals}</strong>
+            </td>
+            <td>
+              <strong>{career.assists}</strong>
+            </td>
+            <td>
+              <strong>{career.points}</strong>
+            </td>
+            <td>
+              <strong>{career.plusMinus}</strong>
+            </td>
+            <td>
+              <strong>{career.pim}</strong>
+            </td>
+            <td>
+              <strong>{career.powerPlayGoals}</strong>
+            </td>
+            <td>
+              <strong>{career.powerPlayPoints}</strong>
+            </td>
+            <td>
+              <strong>{career.shortHandedGoals}</strong>
+            </td>
+            <td>
+              <strong>{career.shortHandedPoints}</strong>
+            </td>
+            <td>
+              <strong>{career.gameWinningGoals}</strong>
+            </td>
+            <td>
+              <strong>{career.overTimeGoals}</strong>
+            </td>
+            <td>
+              <strong>{career.shots}</strong>
+            </td>
+            <td>
+              <strong>{career.shotPct}</strong>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <style jsx>
+        {`
+          .sort-button {
+            padding: 0;
+            margin: 0;
+            border: none;
+            font-weight: bold;
+            background: transparent;
+          }
+          .sort-button:hover {
+            cursor: pointer;
+          }
+
+          .stat-cell--sorted {
+            background-color: yellow;
+          }
+        `}
+      </style>
+    </React.Fragment>
+  );
+};
 
 SkaterTable.propTypes = {
-  stats: PropTypes.arrayOf(PropTypes.shape({
-    season: PropTypes.string,
-  })),
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      season: PropTypes.string,
+    }),
+  ),
   career: PropTypes.shape({
     stat: PropTypes.shape({
       timeOnIce: PropTypes.string,
